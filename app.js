d21f94f4466a2f95589bf2c08c5b7eda08201c2b
adhil-dataspot <adhilashraf76@gmail.com>
2024-02-04 01:19:42 +0530

    adding express-rate-limit for security purpose

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const auth = require('./postController');
+const rateLimit = require('express-rate-limit');
 
 // Middleware to parse JSON
 app.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+  });
+app.use(limiter);//to prevent DDos attack
 
 // Router
 app.get('/', (req, res) => {
